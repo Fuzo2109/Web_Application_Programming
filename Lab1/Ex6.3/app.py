f545747f2e2ff4a7c102a06061fd8221f5e96a22
FuZo <koi0905phu@gmail.com>
2025-05-26 08:12:03 +0700

    First commit

--- Lab1/Ex6.3/app.py
+++ Lab1/Ex6.3/app.py
@@ -6,13 +6,15 @@ app.secret_key = 'your_secret_key'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        student_name == request.form['student_name']
+        username = request.form['username']
         password = request.form['password']
         gender = request.form['gender']
         membership = request.form['membership']
-        favorite_color = request.form['favorite_color']
+        color = request.form['favcolor']
         is_correct = 'correct' in request.form
         flash('Student Information Submmited Successfully!', 'success')
+
+        return f"Submited : {username}, {gender}, {membership}, {color}, Correct: {is_correct}"
     return render_template('index.html')
 
 if __name__ == '__main__':
